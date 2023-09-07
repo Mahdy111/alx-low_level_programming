5c423d050f463e31d97c0cb9f7b9d7b7122b1178
Mahdy111 <abdallah.mahdy231@gmail.com>
2023-09-07 12:18:52 -0700

    hopeee

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+  * Printf for 1st time
+  * it's me again AMAHDY
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+  * main is the only one there
+  * return nothing
+*/
+
+int main(void)
+{
+	printf("Size of a char: %d\n", sizeof(char));
+	printf("Size of an int: %d\n", sizeof(int));
+	printf("Size of a long int: %d\n", sizeof(long int));
+	printf("Size of a long long int: %d\n", sizeof(long long int));
+	printf("Size of a float: %d\n", sizeof(float));
+
+	return (0);
+}
