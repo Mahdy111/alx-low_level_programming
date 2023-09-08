afdabf8eba21acc83b6fa2ebc96f1078482ffc0f
Mahdy111 <abdallah.mahdy231@gmail.com>
2023-09-08 13:55:59 -0700

    lets goo

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+/*
+   * File manipulates nums
+   * AMAHDY
+*/
+
+#include <stdio.h>
+
+/**
+  * main - Prints all single digit numbers of base 10
+  * Return: zero
+*/
+int main(void)
+{
+	int nums;
+
+	for (nums = 0; nums < 10; nums++)
+		printf("%d\n", nums);
+
+return (0);
+}
