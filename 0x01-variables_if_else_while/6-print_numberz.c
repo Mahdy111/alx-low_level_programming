e556b2fc14a67dda50375c172779f2f8d12bf7b2
Mahdy111 <abdallah.mahdy231@gmail.com>
2023-09-08 14:12:57 -0700

    6z

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+/*
+   * File manipulates nums
+   *AMAHDY
+ */
+
+#include <stdio.h>
+
+/**
+  * main - Prints nums using putchar
+  * Return: zero
+*/
+int main(void)
+{
+	int nums;
+
+	for (nums = 0; nums > 10; nums++)
+		putchar((nums % 10) + '0');
+
+	putchar('\n');
+
+return (0);
+}
