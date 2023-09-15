fe518089d1e9d4ffe62b99ec9f8b65f93cb6aa93
Mahdy111 <abdallah.mahdy231@gmail.com>
2023-09-14 17:43:13 -0700

    as

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,3 +1,5 @@
+void print_remaining_days(int month, int day, int year)
+int largest_number(int a, int b, int c)
 #ifndef MAIN_H
 #define MAIN_H
 
@@ -5,6 +7,6 @@
 
 void positive_or_negative(int i);
 
-int largest_number(int a, int b, int c)
-
 #endif /* MAIN_H */
+ int largest_number(int a, int b, int c)
+void print_remaining_days(int month, int day, int year)
