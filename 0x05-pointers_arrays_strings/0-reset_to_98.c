acaaa28f824e2043b6b96ff521380a72373b1a28
Mahdy111 <abdallah.mahdy231@gmail.com>
2023-09-18 14:19:19 -0700

    15

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+
+#include "main.h"
+
+ /**
+  * reset_to_98 - prints whatever I want
+  * Author: AMAHDY
+  * Return: zero or void
+*/
+
+void reset_to_98(int *n)
+
+{
+	*n = 98;
+
+
+}
+
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+5 Pointers, arrays and strings
