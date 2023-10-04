7d04362fc7456cd2241adc883eca84b51a318cc8
Mahdy111 <abdallah.mahdy231@gmail.com>
2023-10-03 23:39:13 -0700

    lib

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,10 +9,12 @@
  * @s2: input string
  * Return: pointer to a newly allocated space in memory, else NULL
  */
+
 char *str_concat(char *s1, char *s2)
 {
 	char *ch;
 	int i, j;
+
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
