4c40cafa6ecd0165fc0e1195a98cd5ff6d1603e8
Mahdy111 <abdallah.mahdy231@gmail.com>
2023-10-11 10:21:58 -0700

    F to P

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,9 +1,10 @@
-#ifndef _FUNCTION_POINTERS_H_
-#define _FUNCTION_POINTERS_H_
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
 /**
  * File: main
  * Desc: header file
-*/
+ */
+int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
