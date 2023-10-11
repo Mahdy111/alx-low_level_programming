5adb4e99e4ea2507862ad2ad4a053879d965adaf
Mahdy111 <abdallah.mahdy231@gmail.com>
2023-10-11 10:18:25 -0700

    F to P

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,20 @@
-#include "function_pointers.h"
 #include <stdlib.h>
+#include <stdio.h>
+#include "function_pointers.h"
 /**
  * print_name - prints a name
  * @name: name of the person
  * @f: is pointer
-*/
+ */
 
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
+	{
 		return;
-	f(name);
+	}
+	else
+	{
+		f(name);
+	}
 }
