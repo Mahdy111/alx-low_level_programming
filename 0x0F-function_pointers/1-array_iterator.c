405f6e94ba783152b64192136535a9887dbc9a15
Mahdy111 <abdallah.mahdy231@gmail.com>
2023-10-11 10:27:20 -0700

    F to P

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+  * array_iterator - initializes a variable
+  *
+  * @array: name of the array
+  * @size: a Size
+  * @action: pointer to function
+  * Return: Zero
+  */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (array == NULL || size <= 0 || action == NULL)
+	{
+		return;
+	}
+	for (i = 0; i < size; i++)
+	{
+		(*action)(array[i]);
+	}
+}
