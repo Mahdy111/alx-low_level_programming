519620783ef21731381800d89f85dd1349ae1534
Mahdy111 <abdallah.mahdy231@gmail.com>
2023-10-11 09:18:27 -0700

    macro

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,10 +1,9 @@
 #ifndef SUMMACRO_H
 #define SUMMACRO_H
-#define SUMMACRO(x, y) (x + y)
 /**
- * File: computes the sum of the numbers x and yi
- * @x: input
- * @y: input
+ * File: computes the sum of the numbers x and y
  * Desc: header file
  */
+#define SUMMACRO(x, y) (x + y)
+
 #endif
