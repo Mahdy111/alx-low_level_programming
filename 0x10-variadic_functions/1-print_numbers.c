8acda4dd11c0f36e8d81257e96af4b1c6b45744c
Mahdy111 <abdallah.mahdy231@gmail.com>
2023-10-12 06:41:02 -0700

    varargas

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_numbers - prints numbers, followed by a new line
+ * @n: int value
+ * @separator: char
+ * Return: Zero
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list valist;
+	unsigned int i;
+
+	va_start(valist, n);
+	for (i = 0 ; i < n ; i++)
+	{
+		printf("%d", va_arg(valist, int));
+		if (separator != 0 && i < n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
+	va_end(valist);
+	printf("\n");
+}
