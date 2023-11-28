43fe75a25249f246d79f724d2e90221c60c074c0
root <abdallahmahdy231@gmail.com>
2023-11-27 23:21:12 -0800

    FILE

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,30 +8,30 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int i, j, fb = 0;
+	int fb;
+	int let;
+	int per;
 
 	if (!filename)
-	{
 		return (-1);
-	}
-	i = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	fb = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
 	if (fb == -1)
-	{
 		return (-1);
-	}
-	if (text_content)
-	{
-		while (text_content[fb] != '\0')
-		{
-			fb++;
-		}
-		j = write(i, text_content, fb);
-	}
-	if (j == -1 || j != fb)
-	{
-		close(i);
+
+	if (!text_content)
+		text_content = "";
+
+	for (let = 0; text_content[let]; let++)
+		;
+
+	per = write(fb, text_content, let);
+
+	if (per == -1)
 		return (-1);
-	}
-	close(i);
+
+	close(fb);
+
 	return (1);
 }
